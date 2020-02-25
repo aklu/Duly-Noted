2eb3c89c47959698625500ebb1d46d081380a2d8
aklu <amandaklusmeyer@gmail.com>
2020-02-24 21:50:00 -0600

    week 5 changes

--- src/components/NoteEditPage.js
+++ src/components/NoteEditPage.js
@@ -1,24 +1,72 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { 
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonActionSheet
+} from "@ionic/react";
+import { chevronBack, ellipsisHorizontal, trash, close} from "ionicons/icons";
+import styles from "./NoteEditPage.module.css";
 
  export default function NoteEditPage(props) {
-    const { onSave, text, onCancel, onDelete } = props;
+    const { 
+      onSave, 
+      text, 
+      onDelete 
+    } = props;
     const [value, setValue] = useState(text);
-    
-    return(
-      <div className="page">
-        <h1>Note Edit</h1>
-        <textarea value={value} onChange={(event) => setValue(event.target.value)}/>
-        <button type="button" onClick={() => onSave(value)}>Save</button>
-        <button type="button" onClick={() => onCancel()}>Cancel</button>
-        <button type="button" onClick={() => onDelete()}>Delete</button>
-      </div>
-    );
+    const [showActions, setShowActions] = useState(false);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="secondary">
+            <IonButton color="secondary" onClick={() => onSave(value)}>
+              <IonIcon slot="icon-only" icon={chevronBack} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>Note Edit</IonTitle>
+          <IonButtons slot="primary">
+            <IonButton color="secondary" onClick={() => setShowActions(true)}>
+              <IonIcon slot="icon-only" icon={ellipsisHorizontal}/>
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <textarea className={styles.textArea} value={value} onChange={(event) => setValue(event.target.value)}/>
+        <IonActionSheet 
+          isOpen={showActions}
+          onDidDismiss={() => setShowActions(false)}
+          buttons={[
+            {
+              text: "Delete",
+              role: "destructive",
+              icon: trash,
+              handler: onDelete
+            },
+            {
+              text: "Cancel",
+              role: "cancel",
+              icon: close,
+              handler: () => setShowActions(false)
+            }
+          ]}
+        />
+      </IonContent>
+    </IonPage>
+  );
   }
 
   NoteEditPage.propTypes = {
     onSave: PropTypes.func.isRequired,
-    onCancel: PropTypes.func.isRequired,
     onDelete: PropTypes.func.isRequired,
     text: PropTypes.string.isRequired
   };
